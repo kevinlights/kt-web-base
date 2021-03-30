d3c4419e660541cf4b5c65f8af5fad4c92203783
guangjh <guangjinhai@163.com>
2021-03-30 23:34:45 +0800

    optimize and add spring boot framework

--- kt-springboot-demo/src/main/kotlin/com/gjh/learn/kt/controller/HelloController.kt
+++ kt-springboot-demo/src/main/kotlin/com/gjh/learn/kt/controller/HelloController.kt
@@ -3,6 +3,7 @@ package com.gjh.learn.kt.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 /**
  *
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
 
     @GetMapping("/index")
-    fun index(): String {
-        return "Hello World!"
+    fun index(req: HttpServletRequest): String {
+        return "Hello World! from ${req.requestURI}"
     }
 }
